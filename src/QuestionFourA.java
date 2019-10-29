2a603a659c352cf1da73d0c07fe9dd74847e279c
azmirfakkri <azmirfakkri@gmail.com>
2019-10-29 16:26:28 +0000

    update q4a

--- src/QuestionFourA.java
+++ src/QuestionFourA.java
@@ -15,8 +15,40 @@ public class QuestionFourA {
         // printing arrays in arrayList
         System.out.println(Arrays.deepToString(listOfAnswersFromEveryone.toArray()));
 
-        // calculate the percentage - another method
-        System.out.println(Arrays.toString(listOfAnswersFromEveryone.get(0)));
+        // sort these answers into 3 arrays of 4 answers - another method
+        String[] question1 = new String[4];
+        String[] question2 = new String[4];
+        String[] question3 = new String[4];
+
+        question1[0] = listOfAnswersFromEveryone.get(0)[0];
+        question2[0] = listOfAnswersFromEveryone.get(0)[1];
+        question3[0] = listOfAnswersFromEveryone.get(0)[2];
+
+        question1[1] = listOfAnswersFromEveryone.get(1)[0];
+        question2[1] = listOfAnswersFromEveryone.get(1)[1];
+        question3[1] = listOfAnswersFromEveryone.get(1)[2];
+
+        question1[2] = listOfAnswersFromEveryone.get(2)[0];
+        question2[2] = listOfAnswersFromEveryone.get(2)[1];
+        question3[2] = listOfAnswersFromEveryone.get(2)[2];
+
+        question1[3] = listOfAnswersFromEveryone.get(3)[0];
+        question2[3] = listOfAnswersFromEveryone.get(3)[1];
+        question3[3] = listOfAnswersFromEveryone.get(3)[2];
+
+        System.out.println(Arrays.toString(question1));
+        System.out.println(Arrays.toString(question2));
+        System.out.println(Arrays.toString(question3));
+
+        // calculate
+        double counter = 0;
+        for (String s : question1) {
+            if (s.equals("yes")) {
+                counter++;
+            }
+        }
+        double total = (counter / 4) * 100;
+        System.out.println(total);
     }
 
     private static String[] surveyQuestions(int person) {
@@ -41,7 +73,7 @@ public class QuestionFourA {
                 }
             } while (!answer.equals("yes") && !answer.equals("no"));
 
-            // add every answer into answers array
+            // add every answer into answers array - [yes, yes, yes]
             answers[q] = answer;
 
         }
