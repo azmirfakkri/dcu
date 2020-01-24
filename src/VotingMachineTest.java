f3e18bd9fa9c51ab33296974ba42cc4635671055
azmirfakkri <azmirfakkri@gmail.com>
2020-01-24 11:12:02 +0000

    add comment q3

--- src/VotingMachineTest.java
+++ src/VotingMachineTest.java
@@ -2,9 +2,10 @@ public class VotingMachineTest {
 
     public static void main(String[] args) {
 
-        // for loop 3 times
+        // for loop 3 times to do election
         for (int i = 0; i < 3; i++) {
 
+            // get number of votes
             int partyA = VotingMachine.voteRandomizer();
             int partyB = VotingMachine.voteRandomizer();
 
@@ -14,8 +15,10 @@ public class VotingMachineTest {
 
             int[] votes = new int[]{partyA, partyB};
 
+            // count votes
             VotingMachine.voteCounter(votes);
 
+            // clear votes
             VotingMachine.clearVotes(votes);
 
         }
